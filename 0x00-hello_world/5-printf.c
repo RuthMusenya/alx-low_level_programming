ebd7874c783d4199cf9cb1b00214832aa1d126ae
Your Name <you@example.com>
2023-03-16 15:12:40 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main - main function
  *
- * Return: 0 
+ * Return: 0
  */
 
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,\n")
-		return (0)
+        printf("\"with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
 
